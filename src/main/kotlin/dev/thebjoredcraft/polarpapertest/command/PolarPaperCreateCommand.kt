995a2791f2a11e6f5bb659086d50e18670ecd7d8
Ammo <ammo@slne.dev>
2025-03-09 13:45:44 +0100

    feat: enhance PolarPaperCreateCommand to measure and report world creation time

--- src/main/kotlin/dev/thebjoredcraft/polarpapertest/command/PolarPaperCreateCommand.kt
+++ src/main/kotlin/dev/thebjoredcraft/polarpapertest/command/PolarPaperCreateCommand.kt
@@ -5,31 +5,50 @@ import dev.jorel.commandapi.CommandAPICommand
 import dev.jorel.commandapi.arguments.IntegerArgument
 import dev.jorel.commandapi.executors.PlayerCommandExecutor
 import dev.thebjoredcraft.polarpapertest.plugin
-
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import live.minehub.polarpaper.Polar
 import net.kyori.adventure.text.Component
-
 import org.bukkit.WorldCreator
+import java.util.*
+import kotlin.system.measureTimeMillis
 
-import java.util.UUID
-
-class PolarPaperCreateCommand(commandName: String): CommandAPICommand(commandName) {
+class PolarPaperCreateCommand(commandName: String) : CommandAPICommand(commandName) {
     init {
-        withArguments(IntegerArgument("amount"))
+        withOptionalArguments(IntegerArgument("amount"))
 
         executesPlayer(PlayerCommandExecutor() { player, args ->
-            val amount: Int = args.getUnchecked<Int>("amount") ?: return@PlayerCommandExecutor
+            val amount = args.getOrDefaultUnchecked("amount", 1)
+            val times = mutableListOf<Long>()
 
             plugin.launch {
-                val start = System.currentTimeMillis()
+                withContext(Dispatchers.IO) {
+                    val totalTime = measureTimeMillis {
+                        repeat(amount) {
+                            times.add(measureTimeMillis {
+                                Polar.createPolarWorld(
+                                    WorldCreator.name(UUID.randomUUID().toString())
+                                )
+                            })
+                        }
+                    }
 
-                repeat(amount) {
-                    Polar.createPolarWorld(WorldCreator.name(UUID.randomUUID().toString()))
+                    val minTime = times.minOrNull()
+                    val avgTime = times.average()
+                    val maxTime = times.maxOrNull()
+
+                    val builder = Component.text()
+                    builder.append(Component.text("Created $amount worlds in $totalTime ms"))
+                    builder.append(Component.newline())
+                    builder.append(Component.newline())
+                    builder.append(Component.text("Min: $minTime ms"))
+                    builder.append(Component.newline())
+                    builder.append(Component.text("Avg: $avgTime ms"))
+                    builder.append(Component.newline())
+                    builder.append(Component.text("Max: $maxTime ms"))
+
+                    player.sendMessage(builder.build())
                 }
-
-                val ending = System.currentTimeMillis()
-
-                player.sendMessage(Component.text("Created $amount polar worlds in ${ending - start}ms"))
             }
         })
     }
